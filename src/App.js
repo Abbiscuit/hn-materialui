de7d02d754cd5c339e7811fd9a8666557a24695c
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2020-03-02 10:26:55 +0900

    Add Drawer & AppBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import AppDrawer from "./components/AppDrawer";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 function App() {
   return (
     <div className="App">
-      <CssBaseline />
+      <AppDrawer>
+        <CssBaseline />
+        <h1>Hello World</h1>
+      </AppDrawer>
     </div>
   );
 }
---
+++ src/components/AppDrawer.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import Header from "./Header";
+
+const useStyles = makeStyles({
+  list: {
+    width: 250
+  },
+  fullList: {
+    width: "auto"
+  }
+});
+
+const AppDrawer = ({ children }) => {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false
+  });
+
+  const toggleDrawer = (side, open) => event => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [side]: open });
+  };
+
+  const sideList = side => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(side, false)}
+      onKeyDown={toggleDrawer(side, false)}
+    >
+      <List>
+        {["商品リスト", "お気にいり", "プロフィール", "商品カート"].map(
+          (text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          )
+        )}
+      </List>
+      {/* <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List> */}
+    </div>
+  );
+
+  return (
+    <div>
+      <Header toggleDrawer={toggleDrawer} />
+      <SwipeableDrawer
+        open={state.left}
+        onClose={toggleDrawer("left", false)}
+        onOpen={toggleDrawer("left", true)}
+      >
+        {sideList("left")}
+      </SwipeableDrawer>
+
+      {children}
+    </div>
+  );
+};
+
+export default AppDrawer;
